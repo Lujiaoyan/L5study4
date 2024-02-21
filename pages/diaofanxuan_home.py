f47e876a43edac2a2f2ae6acf6a528d0276d6e52
Lujiaoyan <153267639+Lujiaoyan@users.noreply.github.com>
2024-02-21 14:07:01 +0800

    Update diaofanxuan_home.py

--- pages/diaofanxuan_home.py
+++ pages/diaofanxuan_home.py
@@ -1,7 +1,8 @@
 '''我的主页'''
 import streamlit as st
+from PIL import Image
 
-page = st.sidebar.radio('我的首页',['原神目前版本新角色介绍','我的图片处理工具','我的留言区'])
+page = st.sidebar.radio('我的首页',['原神目前版本新角色介绍','我的快速“更改神之眼大法”','我的留言区'])
 
 def page1():
     '''我的兴趣'''
@@ -12,8 +13,7 @@ def page1():
     with open('下一个远方.mp3','rb') as f:
         mymp3 = f.read()
     st.audio(mymp3, format='audio/mp3',start_time=0)
-    st.image('4.4官方pv宣传图.png')
-    st.image('4.4新角色——闲云.png')
+    st.image('4.4官方pv宣传图.png')    
     st.image('4.4新角色——嘉明.png')
     st.image('嘉明培养攻略.png')
     st.write('作者：小橙子猪弟,抖音号：1139838392')
@@ -24,22 +24,50 @@ def page1():
     st.write('No3.“砂罗班嘉”（嘉明+砂糖+罗莎莉亚+班尼特）'+':orange[备注：造价便宜弥补画地为牢的缺点]')
     st.write('No4.“芙嘉闲坎”（嘉明+芙宁娜+闲云+坎蒂丝）'+':orange[备注：‘小鸟芙特嘉’的变种版，坎不用练度，操作无脑]')
     st.write('No5.嘉明纯火队（嘉明+万叶+香菱+班尼特）'+':orange[备注：嘉明版的国家队]')
+    st.image('4.4新角色——闲云.png')
     st.image('闲云培养攻略.png')
     st.write('作者：苏打ooo,抖音号：2226401033')
-    st.write(':orange[那个女人没抽出来，没有研究...先看攻略吧（手动尴尬]')
+    st.write(':orange[那个女人没抽出来，没有研究...先看攻略吧（手动尴尬）]')
     
 def page2():
-    '''图片处理工具'''
-    pass
+    '''我的图片处理工具'''
+    st.write(':sunglasses:最近网上总有人给角色换色就当换元素了，于是我直接提供一个快速方式:sunglasses:')
+    uploaded_file = st.file_uploader('上传图片', type=['png', 'jpeg', 'jpg'])
+    if uploaded_file:
+        file_name = uploaded_file.name
+        file_type = uploaded_file.type
+        file_size = uploaded_file.size
+        img = Image.open(uploaded_file)
+        tab1, tab2, tab3, tab4 = st.tabs(['原图', '改色1', '改色2', '改色3'])
+        with tab1:
+            st.image(img)
+        with tab2:
+            st.image(img_change(img, 0, 2, 1))
+        with tab3:
+            st.image(img_change(img, 1, 2, 0))
+        with tab4:
+            st.image(img_change(img, 1, 0, 2))        
     
 def page3():
     '''留言区'''
     pass
 
+def img_change(img, rc, gc, bc):
+    '''图片处理'''
+    width, height = img.size
+    img_array = img.load()
+    for x in range(width):
+        for y in range(height):
+            r = img_array[x, y][rc]
+            g = img_array[x, y][gc]
+            b = img_array[x, y][bc]
+            img_array[x, y] = (r, g, b)
+    return img
+
 if page == '原神目前版本新角色介绍':
     page1()
 
-elif page == '我的图片处理工具':
+elif page == '我的快速“更改神之眼大法”':
     page2()
 
 elif page == '我的留言区':
