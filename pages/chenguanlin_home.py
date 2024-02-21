f18df9afc5a032f9cb789a72b1441cef7b85f3a3
Lujiaoyan <153267639+Lujiaoyan@users.noreply.github.com>
2024-02-21 15:31:34 +0800

    Update chenguanlin_home.py

--- pages/chenguanlin_home.py
+++ pages/chenguanlin_home.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from PIL import Image
+#python -m  streamlit run C:\Users\shugu\Desktop\
+陈冠霖的网络根据地\chenguanlin_home.py
 
 
 page=st.sidebar.radio('我的首页',['我的兴趣推荐','我的图片处理工具','我的智能词典','我的留言区'])
@@ -9,7 +12,16 @@ def page1():
     pass
 def page2():
     '''图片处理工具'''
-    pass
+    st.write(":sunglasses:图片处理小程序:sunglasses:")
+    uploaded_file=st.file_uploader("上传图片",type=['png','jpeg','jpg'])
+    if uploaded_file: 
+        file_name=uploaded_file.name
+        file_type=uploaded_file.type
+        file_size=uploaded_file.size
+        img=Image.open(uploaded_file)
+        st.image(img)
+        st.image(img_change(img,0,2,1))
+        
 def page3():
     '''智能词典'''
     pass
@@ -18,7 +30,7 @@ def page4():
     pass
 if page=='我的兴趣推荐':
     page1()
-    st.image('bc0594dc-ca0f-4c44-ac2d-df93d9eab3e6.jpg')
+    st.image("bc0594dc-ca0f-4c44-ac2d-df93d9eab3e6.jpg")
     st.write("九品芝麻官")
 elif page=='我的图片处理工具':
     page2()
@@ -28,3 +40,23 @@ elif page=='我的智慧词典':
 elif page=='我的留言区':
     page4()
     st.write()
+def img_change(img,rc,gc,bc):
+    '''图片处理'''
+    width,height=img.size
+    img_array=img.load()
+    for x in range(width):
+        for y in range(height):
+            r=img_array[x,y][rc]
+            g=img_array[x,y][gc]
+            b=img_array[x,y][bc]
+            img_array[x,y]=(r,g,b)
+    return img
+tab1,tab2,tab3,tab4=st.tabs(["原图","改色1","改色2","改色3"])
+with tab1
+    st.image(img)
+with tab2
+    st.image(img_change(img,0,2,1)))
+with tab3
+    st.image(img_change(img,1,2,0))
+with tab4
+    st.image(img_change(img,1,0,2))      
